f079fb3a94946bddd6368e80959b5fc299bf1f18
RedSponge <eran.godsi2003@gmail.com>
2019-03-30 00:06:51 +0300

    Document ImageUtils

--- app/src/main/java/com/redsponge/mycoolapp/utils/ImageUtils.java
+++ app/src/main/java/com/redsponge/mycoolapp/utils/ImageUtils.java
@@ -9,11 +9,22 @@ import java.io.ByteArrayOutputStream;
 
 public class ImageUtils {
 
+    /**
+     * Decodes a base64 string into a {@link Bitmap}
+     * @param base64 The base64 string to decode (usually received from {@link com.redsponge.mycoolapp.db.DatabaseHandler#getIcon(int)}
+     * @return The bitmap
+     */
     public static Bitmap decode(String base64) {
         byte[] arr = Base64.decode(base64, Base64.DEFAULT);
         return BitmapFactory.decodeByteArray(arr, 0, arr.length);
     }
 
+    /**
+     * Scales an image down to be stored in the database and later displayed
+     * Unless needed the source bitmap should be recycled with {@link Bitmap#recycle()}
+     * @param bmp The bitmap to scale down
+     * @return The scaled down bitmap
+     */
     public static Bitmap scaleDown(Bitmap bmp) {
         float scalar = 1;
         while(bmp.getWidth() * scalar > Constants.MAX_IMAGE_SIZE) {
@@ -22,8 +33,12 @@ public class ImageUtils {
         return Bitmap.createScaledBitmap(bmp, (int) (bmp.getWidth() * scalar), (int) (bmp.getHeight() * scalar), false);
     }
 
+    /**
+     * Encodes a bitmap into a base64 string to store in the database
+     * @param bmp The bitmap to encode
+     * @return The encoded string
+     */
     public static String encode(Bitmap bmp) {
-
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         bmp.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
         byte[] byteArr = byteArrayOutputStream.toByteArray();
